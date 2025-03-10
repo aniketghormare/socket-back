4d3af148a32913d1f447df52dd669435f7dffbde
Aniket Ghormare <aniketghormare62@gmail.com>
2025-03-10 16:55:18 +0530

    abc

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: [
+            "http://localhost:5173",     // Vite (Local)
+            "https://your-frontend.vercel.app", // Vercel (Production)
+            "https://your-backend.onrender.com" // Render (Production)
+        ],
+        methods: ["GET", "POST"],
+    },
+});
+
+
+io.on("connection", (socket) => {
+    console.log(`User connected: ${socket.id}`);
+
+    socket.on("join_room", (room) => {
+        socket.join(room);
+        console.log(`User ${socket.id} joined room: ${room}`);
+    });
+
+    socket.on("send_message", (data) => {
+        const { room, message, username } = data;
+        io.to(room).emit("receive_message", { message, username }); // Broadcast only to that room
+    });
+
+    socket.on("disconnect", () => {
+        console.log(`User disconnected: ${socket.id}`);
+    });
+});
+
+let PORT=4000 || process.env.PORT;
+
+server.listen(PORT, () => console.log("Server running on port 4000"));
